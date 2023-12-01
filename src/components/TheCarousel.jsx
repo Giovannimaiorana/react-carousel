8e2d56571d7d91daa565b16528bf5706a1fbd57a
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-12-01 16:14:25 +0100

    esercizio completo più bonus

--- src/components/TheCarousel.jsx
+++ src/components/TheCarousel.jsx
@@ -4,38 +4,58 @@ import img1 from "../assets/fotouno.jpg";
 import img2 from "../assets/fotodue.jpg";
 import img3 from "../assets/fototre.jpg";
 import img4 from "../assets/fotoquattro.jpg";
-
+import img5 from "../assets/img5.jpg";
 const slides = [
   {
     image: img1,
-    text: "testo a caso",
+    text: "SAMU MA",
   },
   {
     image: img2,
-    text: "testo a caso",
+    text: "SECONDO",
   },
   {
     image: img3,
-    text: "testo a caso",
+    text: "TE",
   },
   {
     image: img4,
-    text: "testo a caso",
+    text: "ENTRA?",
+  },
+  {
+    image: img5,
+    text: "NON ENTRA",
   },
 ];
 
 export default function TheCarousel() {
+  //usestate
   const [currentSlide, setCurrentSlide] = useState(0);
-
+  //function per andare avanti
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
   };
-
+  //function per andare indietor
   const prevSlide = () => {
     setCurrentSlide((prevSlide) =>
       prevSlide === 0 ? slides.length - 1 : prevSlide - 1
     );
   };
+  //useEffect() x timer
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [currentSlide]);
+  //funzione per bullet
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
+
   return (
     <div className={style.carouselContainer}>
       <div className={style.carouselElement}>
@@ -44,12 +64,24 @@ export default function TheCarousel() {
           src={slides[currentSlide].image}
           alt={`Slide ${currentSlide + 1}`}
         />
+        <p className={style.testoCard}>{slides[currentSlide].text}</p>
         <button className={style.buttonPrev} onClick={prevSlide}>
           &lt;
         </button>
         <button className={style.buttonNext} onClick={nextSlide}>
           &gt;
         </button>
+        <div className={style.bulletsContainer}>
+          {slides.map((slide, index) => (
+            <div
+              className={
+                index === currentSlide ? style.activeBullet : style.bullets
+              }
+              key={index}
+              onClick={() => goToSlide(index)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
